f898b19ecd7f0463eab571f1bb46c76ea5d213b7
royvardhan <royvardhan.official@gmail.com>
2024-03-16 13:32:07 +0530

    feat: solution

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use lambdaworks_math::{
+    elliptic_curve::{
+        short_weierstrass::curves::bls12_381::curve::BLS12381Curve,
+        traits::IsEllipticCurve,
+    },
+    msm::naive::msm, 
+    traits::ByteConversion,
+};
+
+fn main() {
+
+    let secret_key: u128 = u128::from_str_radix("6C616D6264617370", 16).unwrap();
+   
+    let g = BLS12381Curve::generator();
+    
+
+    let scalars = vec![secret_key];
+    let base_points = vec![g];
+
+    let public_key_points = msm(&scalars, &base_points);
+
+    let affine_point = public_key_points.unwrap().to_affine();
+    let x_bytes = affine_point.x().to_bytes_be();
+    let y_bytes = affine_point.y().to_bytes_be();
+
+    let mut new_public_key_bytes = Vec::new();
+    new_public_key_bytes.extend_from_slice(&x_bytes);
+    new_public_key_bytes.extend_from_slice(&y_bytes);
+
+    let public_key_bytes = hex::encode(new_public_key_bytes);
+
+    println!("Public Key: {:?}", public_key_bytes); 
+
+}
